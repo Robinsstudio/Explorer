864d38ebbe2943828708aa000d1b71757a412d75
Robinsstudio <robin_1997@hotmail.fr>
2018-12-26 16:11:44 +0100

    Basic context menu display

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ class ExplorerView extends React.Component {
 		super(props);
 		this.state = {
 			folder: props.folder,
-			files: props.files
+			files: props.files,
+			contextMenu: { visible: false }
 		};
 
+		this.handleClick = this.handleClick.bind(this);
+
 		socket.on('FolderResponse', ({folder, files}) => {
-			this.setState({ folder: folder, files: files });
+			this.setState({ folder: folder, files: files, contextMenu: false });
 		});
+	}
+
+	handleClick() {
+		this.setState({ contextMenu: { visible: false } });
 	}
 	
 	handleDoubleClick(file) {
@@ -26,16 +33,27 @@ class ExplorerView extends React.Component {
 			file:	(howMany > 1) ? folder.slice(-howMany, 1-howMany)[0] : folder.slice(-1)
 		});
 	}
+
+	updateContextMenu(file, event) {
+		const { pageX, pageY, screenX, screenY } = event;
+		const x = screenX - window.screenX;
+		const y = screenY - window.screenY;
+		const offset = { x: pageX - x, y: pageY - y };
+		this.setState({ contextMenu: { visible: true, x: x, y: y, offset: offset } });
+		event.stopPropagation();
+		event.preventDefault();
+	}
   
 	buildFileItem(file) {
 		return (
-			React.createElement('div', { 'class': file.type, onDoubleClick: () => this.handleDoubleClick(file) },
+			React.createElement('div', { 'class': file.type, onDoubleClick: () => this.handleDoubleClick(file), onContextMenu: e => this.updateContextMenu(file, e) },
 				React.createElement('div', { 'class': 'label' },
 					React.createElement('span', null, file.name)))
 		);
 	}
 	
 	render() {
+		const contextMenu = this.state.contextMenu;
 		return (
 			React.createElement("div", null,
 				React.createElement("div", { id: "path" },
@@ -43,8 +61,40 @@ class ExplorerView extends React.Component {
 						onClick: () => this.goBack(self.length - index)
 					}, folder), React.createElement('div')])).slice(0, -1)),
 
-				React.createElement("div", { id: "explorer" },
-					this.state.files.map(file => this.buildFileItem(file))))
+				React.createElement("div", { id: "explorer", onClick: this.handleClick },
+					this.state.files.map(file => this.buildFileItem(file))),
+					
+				contextMenu.visible && React.createElement(ContextMenu, { x: contextMenu.x, y: contextMenu.y, offset: contextMenu.offset}))
+		);
+	}
+}
+
+class ContextMenu extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	computePosition() {
+		const { x, y, offset } = this.props;
+		this.element.style.left = `${(this.element.offsetWidth < window.innerWidth - x) ? x + offset.x : x - this.element.offsetWidth + offset.x}px`;
+		this.element.style.top	= `${(this.element.offsetHeight < window.innerHeight - y) ? y + offset.y : y - this.element.offsetHeight + offset.y}px`;
+	}
+	
+	componentDidMount() {
+		this.computePosition();
+	}
+	
+	componentDidUpdate() {
+		this.computePosition();
+	}
+	
+	render() {
+		return (
+			React.createElement("div", { id: "contextMenu", ref: ref => this.element = ref },
+				React.createElement("div", null, "Ouvrir"),
+				React.createElement("div", null, "Renommer"),
+				React.createElement("div", null, "Supprimer"),
+				React.createElement("div", null, "Nouveau dossier"))
 		);
 	}
 }
